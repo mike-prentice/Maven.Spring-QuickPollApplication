e13518cc971c5f10c6f462abe9a6989c67f94960
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-04-23 19:45:48 -0400

    added domain classes

---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/domain/Poll.java
@@ -0,0 +1,47 @@
+package io.zipcoder.tc_spring_poll_application.domain;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Poll {
+
+    @Id
+    @GeneratedValue
+    @Column(name="POLL_ID")
+    public  Long id;
+
+
+    @Column(name = "QUESTION")
+    public String question;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "POLL_ID")
+    @OrderBy
+    public Set<Option> options = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public Set<Option> getOptions() {
+        return options;
+    }
+
+    public void setOptions(Set<Option> options) {
+        this.options = options;
+    }
+}
